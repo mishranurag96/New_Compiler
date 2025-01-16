1357cd4c2eeab73ccd59ceaf8a9d70e50c9b662e
Anurag Mishra <mishranurag96@gmail.com>
2025-01-16 17:40:28 +0530

    Add files via upload

---
+++ main-2.java
@@ -0,0 +1,10 @@
+
+    // Online Free Java compiler to run Java program online
+    import java.util.Scanner;
+    public class Main {
+      public static void main(String[] args) {
+          System.out.println("Enter name");
+          Scanner sc = new Scanner(System.in);
+          String a = nextLine();
+      }
+    }
